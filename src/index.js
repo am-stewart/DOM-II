7f937863236292e621d151c7cd412bf5842ab4f2
Allison Stewart <amstewart@yahoo.com>
2021-11-30 18:42:08 -0500

    cleaned up code, have a mouseover, mouseleave, click, dblclick, wheel, keydown, and load event

--- src/index.js
+++ src/index.js
@@ -4,57 +4,55 @@ import './less/index.less'
 const logo = document.querySelector('.logo-heading');
 const links = document.querySelectorAll('.nav-link')
 const linksArray = Array.from(links);
-const welcomeBus = document.querySelectorAll('h2');
-const welcomeBusArray = Array.from(welcomeBus);
+const sectionH2 = document.querySelectorAll('h2');
+const sectionH2Array = Array.from(sectionH2);
 const images = document.getElementsByTagName('img');
 const imagesArray = Array.from(images);
 const button = document.getElementsByClassName('btn');
 const buttonArray = Array.from(button);
+const paras = document.getElementsByClassName('p');
+const parasArray = Array.from(paras);
+
+//window
+window.addEventListener('load', event => {
+    console.log('PAGE HAS FULLY LOADED');
+})
 
 //logo
 logo.addEventListener('mouseover', event => {
     event.target.style.fontSize = '3rem';
-    event.target.style.color = 'blue';
+    event.target.style.color = 'cyan';
 })
-
 logo.addEventListener('mouseleave', event => {
-    event.target.style.fontSize = '4rem';
-    event.target.style.color = 'black';
+    event.target.style.fontSize = '';
+    event.target.style.color = '';
 })
 
 //links
-linksArray[0].addEventListener('click', event => {
-    event.target.style.color = 'cyan';
-})
-linksArray[1].addEventListener('click', event => {
-    event.target.style.opacity = '50%';
-    console.log('is this working?');
-})
-linksArray[2].addEventListener('click', event => {
-    event.target.style.color = 'cyan';
-})
-linksArray[3].addEventListener('click', event => {
-    event.target.style.color = 'cyan';
+linksArray.forEach((link) => {
+    link.addEventListener('click', event => {
+        event.target.style.color = 'cyan';
+    })
 })
 
-//h4s
-welcomeBusArray[0].addEventListener('mouseover', event => {
-    event.target.style.color = 'green';
-})
-welcomeBusArray[1].addEventListener('mouseover', event => {
-    event.target.style.color = 'orange';
-})
-welcomeBusArray[2].addEventListener('mouseover', event => {
-    event.target.style.color = 'purple';
-})
-welcomeBusArray[3].addEventListener('mouseover', event => {
-    event.target.style.color = 'pink';
+//h2s
+sectionH2Array.forEach((h2) => {
+    h2.addEventListener('dblclick', event => {
+        event.target.style.color = 'purple';
+    })
 })
 
 //images
-imagesArray[0].addEventListener('mouseover', event => {
-    event.target.style.opacity = '50%';
-})
+imagesArray.forEach((img) => {
+    img.addEventListener('wheel', (event) => zoom(event, img))
+});
+let scale = 1;
+function zoom(event, img) {
+    event.preventDefault();
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    img.style.transform = `scale(${scale})`;
+}
 
 //buttons
 buttonArray[0].addEventListener('click', event => {
@@ -65,4 +63,13 @@ buttonArray[1].addEventListener('click', event => {
 })
 buttonArray[2].addEventListener('click', event => {
     alert('Get your bathing suit ready!');
+})
+
+//paragraphs
+
+//keydown
+window.addEventListener('keydown', event => {
+    if (event.key === 'f') {
+        window.scrollTo(0, document.body.scrollHeight);
+    }
 })
